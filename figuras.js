267bdf6145d5215857e3bd398a57ce9cc39e3c76
Alejandro Franco <Alejandro.franco@teaminternational.com>
2022-04-30 00:01:13 -0500

    agregada interaccion con html para que los usuarios puedan ingresar valores

--- figuras.js
+++ figuras.js
@@ -37,3 +37,38 @@ function areaCirculo(radio){
 }
 
 console.groupEnd();
+
+function calcularPerimetroCuadrado(){
+    const  value = document.getElementById("inputCuadrado").value;
+
+    const perimetro = perimetroCuadrado(value);
+
+    alert(perimetro);
+}
+
+function calcularAreaCuadrado(){
+    const value = document.getElementById("inputCuadrado").value;
+
+    const area = areaCuadrado(value);
+
+    alert(area);
+}
+
+function calcularPerimetroTriangulo(){
+    const lado1 = parseInt(document.getElementById("inputLado1").value);
+    const lado2 = parseInt(document.getElementById("inputLado2").value);
+    const base = parseInt(document.getElementById("inputBase").value);
+
+    const perimetro = perimetroTriangulo(lado1, lado2, base);
+
+    alert(perimetro);
+}
+
+function calcularAreaTriangulo(){
+    const base = parseInt(document.getElementById("inputBase").value);
+    const altura = parseInt(document.getElementById("inputAltura").value);
+
+    const area = areaTriangulo(base, altura);
+
+    alert(area);
+}
