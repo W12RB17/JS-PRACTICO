f7718e998563731b3b549876aacaed5f03235ed8
Alejandro Franco <Alejandro.franco@teaminternational.com>
2022-04-29 22:56:48 -0500

    logica modificada para utilizar funciones

--- figuras.js
+++ figuras.js
@@ -1,45 +1,39 @@
 //Codigo cuadrado
 console.group('Cuadrados');
 
-const ladoCuadrado = 5;
-const perimetroCuadrado = ladoCuadrado * 4;
-const areaCuadrado = ladoCuadrado * ladoCuadrado;
+function perimetroCuadrado(lado){
+    return lado * 4;
+}
 
-console.log(`Los lados del cuadrado miden: ${ladoCuadrado}`);
-console.log(`El perimetro del cuadrado es: ${perimetroCuadrado}cm`);
-console.log(`El area del cuadrado es: ${areaCuadrado}cm2`);
+function areaCuadrado(lado){
+    return lado * lado;
+}
 
 console.groupEnd()
 
 //Codigo triangulo
 console.group('Triangulos');
 
-const ladoTriangulo1 = 6;
-const ladoTriangulo2 = 6;
-const baseTriangulo = 4;
-const alturaTriangulo = 5.5;
-const perimetroTriangulo = ladoTriangulo1 + ladoTriangulo2 + baseTriangulo;
-const areaTriangulo = (baseTriangulo * alturaTriangulo) / 2;
+function perimetroTriangulo(lado1, lado2, base){
+    return lado1 + lado2 + base;
+}
 
-console.log(`Los lados del triangulo miden: ${ladoTriangulo1}cm, ${ladoTriangulo2}cm y ${baseTriangulo}cm.`);
-console.log(`La altura del triangulo mide: ${alturaTriangulo}cm.`)
-console.log(`El perimetro del triangulo es: ${perimetroTriangulo}cm.`);
-console.log(`El area del triangulo es: ${areaTriangulo}cm2.`);
+function areaTriangulo(base, altura){
+    return (base * altura) / 2;
+}
 
 console.groupEnd();
 
 //Codigo circulos
 console.group('Circulos');
 
-const radio = 4;
-const diametro= radio * 2;
-const pi = Math.PI;
-const perimetroCirculo = diametro * pi;
-const areaCirculo = (radio * radio) * pi;
+function perimetroCirculo(radio){
+    const diametro = radio * 2;
+    return diametro * Math.PI;
+}
 
-console.log(`El radio del circulo es: ${radio}cm.`);
-console.log(`pi es igual a ${pi}.`);
-console.log(`El perimetro del circulo es: ${perimetroCirculo}cm.`)
-console.log(`El area del circulo es: ${areaCirculo}cm2.`);
+function areaCirculo(radio){
+    return (radio * radio) * Math.PI;
+}
 
 console.groupEnd();
